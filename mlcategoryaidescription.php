b1ac1c996e5bb5c08ca6762584d119328485fed1
matteolavaggi <matteo.lavaggi@pineappleitaly.com>
2026-01-15 13:50:32 +0100

    fix: remove non-existent class from require

--- mlcategoryaidescription.php
+++ mlcategoryaidescription.php
@@ -28,7 +28,6 @@ require_once __DIR__ . '/classes/MlCategoryAiClient.php';
 require_once __DIR__ . '/classes/MlCategoryAiGenerator.php';
 require_once __DIR__ . '/classes/MlCategoryAiJobQueue.php';
 require_once __DIR__ . '/classes/MlCategoryAiPlaceholder.php';
-require_once __DIR__ . '/classes/MlCategoryAiPromptTemplate.php';
 require_once __DIR__ . '/classes/MlCategoryAiRunStats.php';
 
 class Mlcategoryaidescription extends Module
